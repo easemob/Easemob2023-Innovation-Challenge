73664a1b9c0c854a1a64103ea175d1b719bdad34
SoftPro <2625852520@qq.com>
2023-11-23 16:19:30 +0800

    新增aiPrompt表

---
+++ Innovation-Challenge/星河小队-ai群管家/backend/src/main/java/com/xinghe/project/service/AiPromptService.java
@@ -0,0 +1,13 @@
+package com.xinghe.project.service;
+
+import com.xinghe.project.model.entity.AiPrompt;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 26258
+* @description 针对表【ai_prompt(消息)】的数据库操作Service
+* @createDate 2023-11-23 15:51:47
+*/
+public interface AiPromptService extends IService<AiPrompt> {
+
+}
